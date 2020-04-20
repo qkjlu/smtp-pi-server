6141393214a8ae90c23e55ce87c785481345e5bb
lucas mas <Lucas@DESKTOP-UMFN118.mecanicsud.com>
2020-04-20 01:05:44 +0100

    stuff

--- seeders/20200418195258-demo.js
+++ seeders/20200418195258-demo.js
@@ -1,29 +1,41 @@
 "use strict";
-const { Lieux, Entreprises, Camionneurs, Admins, Grutiers, Chantiers, Etapes, ChantierCamionneur, LieuGrutier } = require("../models/MockData");
-const sequelize = require('../models').sequelize;
+const {
+  Lieux,
+  Entreprises,
+  Camionneurs,
+  Admins,
+  Grutiers,
+  Chantiers,
+  Etapes,
+  ChantierCamionneur,
+  LieuGrutier,
+} = require("../models/MockData");
+const sequelize = require("../models").sequelize;
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return Promise.all([
-      queryInterface.bulkInsert("Lieux", Lieux),
-      queryInterface.bulkInsert("Camionneurs", Camionneurs),
-      queryInterface.bulkInsert("Admins", Admins),
-      queryInterface.bulkInsert("Grutiers", Grutiers),
-      queryInterface.bulkInsert("Entreprises", Entreprises)
-    ])
+    return sequelize
+      .sync()
+      .then(
+        Promise.all([
+          queryInterface.bulkInsert("Lieux", Lieux),
+          queryInterface.bulkInsert("Camionneurs", Camionneurs),
+          queryInterface.bulkInsert("Admins", Admins),
+          queryInterface.bulkInsert("Grutiers", Grutiers),
+          queryInterface.bulkInsert("Entreprises", Entreprises),
+        ])
+      )
       .then(queryInterface.bulkInsert("Chantiers", Chantiers))
       .then(queryInterface.bulkInsert("Etapes", Etapes))
       .then(
         Promise.all([
-          queryInterface.bulkInsert(
-            "ChantierCamionneur",
-            ChantierCamionneur
-          ),
+          queryInterface.bulkInsert("ChantierCamionneur", ChantierCamionneur),
           queryInterface.bulkInsert("LieuGrutier", LieuGrutier),
         ])
-      ).catch(err => console.log(err))
+      )
+      .catch((err) => console.log(err));
   },
   down: (queryInterface, Sequelize) => {
-    return sequelize.truncate()
+    return sequelize.drop();
   },
 };
