fd8c52e694c2aec46b28466351e063a1870e4b9b
Lucas <lucas_mas@msn.com>
2020-06-25 14:56:45 +0200

    Fix POST /etapes (etapePrec)

--- routes/etapes.js
+++ routes/etapes.js
@@ -11,7 +11,7 @@ router.get("/", async (req, res, next) => {
 });
 
 router.post("/", async (req, res, next) => {
-  const { id, dateDebut, type, CamionneurId, ChantierId, etapePrec } = req.body;
+  const { id, dateDebut, type, CamionneurId, ChantierId, etapePrecId } = req.body;
   if (!(dateDebut && type && CamionneurId && ChantierId)) {
     res.sendStatus(400);
   }
@@ -23,7 +23,7 @@ router.post("/", async (req, res, next) => {
         type,
         CamionneurId,
         ChantierId,
-        etapePrec
+        etapePrecId
       })
     );
   } catch (error) {
