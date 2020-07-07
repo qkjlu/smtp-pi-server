5045593128caa6c7e1f37d4e41b4fb5bd7a0d035
Polyrayan <32355939+Polyrayan@users.noreply.github.com>
2020-07-07 19:04:19 +0200

    temps moyen(average) + etapes(data) + meilleur + pire

--- routes/etapes.js
+++ routes/etapes.js
@@ -1,5 +1,10 @@
+
+
 const router = require("express").Router();
 const Etape = require("../models").sequelize.model('Etape');
+const Camionneur = require("../models").sequelize.model("Camionneur");
+const Chantier = require("../models").sequelize.model("Chantier");
+
 
 router.get("/", async (req, res, next) => {
     try {
@@ -42,4 +47,229 @@ router.patch("/:id", async (req, res, next) => {
   }
 });
 
-module.exports = router;
+router.get("/:id", async (req, res, next) => {
+    const { id  } = req.params;
+    try {
+        const etape = await Etape.findByPk(id)
+        res.json(etape);
+    } catch (error) {
+        next(error)
+    }
+});
+/* ---- Statistics ---- */
+
+function calculSumTimesEtapes(etapes) {
+    let sum = 0;
+    let invalidData = 0;
+    for (let i = 0; i < etapes.length; i++) {
+        let time = new Date(etapes[i].dateFin).getTime() - new Date(etapes[i].dateDebut).getTime()
+        let secondes = Math.floor(time / 1000);
+        if (secondes < 0) {
+            console.log(" id a delete : " + etapes[i].id + " temps = " + secondes)
+            invalidData++
+        } else {
+            console.log(secondes)
+            sum += secondes
+        }
+    }
+    return [sum, invalidData];
+}
+
+async function describeEtapes(etapes){
+    for (let i = 0; i < etapes.length; i++) {
+        console.log(etapes[i])
+        etapes[i]({"camionneur" : await Camionneur.findByPk(etapes[i].CamionneurId)});
+        //etapes[i]["chantier"] =  await Chantier.findByPk(etapes[i].ChantierId);
+    }
+    return etapes;
+}
+
+async function getEtapes(type, ChantierId, CamionneurId, date) {
+    let etapes = null;
+    if(date == null){
+        if(CamionneurId == null){
+            if(ChantierId == null){
+                etapes = await Etape.findAll({where: { type } })
+            }else{
+                etapes = await Etape.findAll({where: { type , ChantierId } })
+            }
+        }else{
+            etapes = await Etape.findAll({where: { type , ChantierId , CamionneurId } })
+        }
+        // date =/= null
+    }else{
+        if(CamionneurId == null){
+            if(ChantierId == null) {
+                etapes = await Etape.findAll({ where: { type, dateDebut: {[Op.gte]: date}, dateFin: {[Op.lte]: date} } })
+            }else{
+                etapes = await Etape.findAll({ where: { type, ChantierId, dateDebut: {[Op.gte]: date}, dateFin: {[Op.lte]: date} } })
+            }
+        }else{
+            etapes = await Etape.findAll({ where: { type, ChantierId, CamionneurId, dateDebut: {[Op.gte]: date}, dateFin: {[Op.lte]: date} } })
+        }
+    }
+    return etapes;
+}
+
+function getBestTime(etapes){
+    let bestTime = -1
+    let bestEtape = null
+    for (let i = 0; i < etapes.length; i++) {
+        let time = Math.floor((new Date(etapes[i].dateFin).getTime() - new Date(etapes[i].dateDebut).getTime())/1000);
+        if (time > 0 && bestTime < 0 || (time > 0 && bestTime > time) ){
+            bestTime = time
+            bestEtape = etapes[i]
+        }
+    }
+    return {
+        bestTime : bestTime,
+        bestEtape : bestEtape
+    };
+}
+
+function getWorstTime(etapes){
+    let worstTime = -1
+    let worstEtape = null
+    for (let i = 0; i < etapes.length; i++) {
+        let time = Math.floor((new Date(etapes[i].dateFin).getTime() - new Date(etapes[i].dateDebut).getTime())/1000);
+        if (time > 0 && worstTime < 0 || (time > 0 && worstTime < time) ){
+            worstTime = time
+            worstEtape = etapes[i]
+        }
+    }
+    return {
+        worstTime : worstTime,
+        worstEtape : worstEtape
+    };
+}
+
+router.get("/:type/average/chantiers/", async (req, res, next) => {
+    const { type  } = req.params;
+    const { date } = req.body;
+    try {
+        const etapes = await getEtapes(type,null,null,date)
+        let result = calculSumTimesEtapes(etapes);
+        let sum = result[0]
+        let invalidData = result[1]
+        let moyenne = Math.floor(sum/(etapes.length - invalidData));
+        const res = {
+            time : moyenne,
+        }
+        res.json(res);
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.get("/:type/average/chantiers/:ChantierId/camionneurs", async (req, res, next) => {
+    const { type , ChantierId  } = req.params;
+    const { date } = req.body;
+    try {
+        const etapes = await getEtapes(type,ChantierId,null,date)
+        let result = calculSumTimesEtapes(etapes);
+        let sum = result[0]
+        let invalidData = result[1]
+        let moyenne = Math.floor(sum/(etapes.length - invalidData));
+        res.json({time : moyenne});
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.get("/:type/average/chantiers/:ChantierId/camionneurs/:CamionneurId", async (req, res, next) => {
+    const { type , ChantierId , CamionneurId } = req.params;
+    const { date } = req.body;
+    try {
+        const etapes = await getEtapes(type,ChantierId,CamionneurId,date)
+        let result = calculSumTimesEtapes(etapes);
+        let sum = result[0]
+        let invalidData = result[1]
+        let moyenne = Math.floor(sum/(etapes.length - invalidData));
+        res.json({time : moyenne});
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.get("/:type/data/chantiers/", async (req, res, next) => {
+    const { type } = req.params;
+    const { date } = req.body;
+    try {
+        const etapes = await getEtapes(type,null,null,date)
+        res.json({etapes : etapes});
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.get("/:type/data/chantiers/best", async (req, res, next) => {
+    const { type } = req.params;
+    const { date } = req.body;
+    try {
+        const etapes = await getEtapes(type,null,null,date)
+        const best = getBestTime(etapes)
+        res.json({best});
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.get("/:type/data/chantiers/:ChantierId/best", async (req, res, next) => {
+    const { type , ChantierId } = req.params;
+    const { date } = req.body;
+    try {
+        const etapes = await getEtapes(type,ChantierId,null,date)
+        const best = getBestTime(etapes)
+        res.json({best});
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.get("/:type/data/chantiers/worst", async (req, res, next) => {
+    const { type } = req.params;
+    const { date } = req.body;
+    try {
+        const etapes = await getEtapes(type,null,null,date)
+        const worst = getWorstTime(etapes)
+        res.json({worst});
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.get("/:type/data/chantiers/:ChantierId/worst", async (req, res, next) => {
+    const { type , ChantierId } = req.params;
+    const { date } = req.body;
+    try {
+        const etapes = await getEtapes(type,ChantierId,null,date)
+        const worst = getWorstTime(etapes)
+        res.json({worst});
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.get("/:type/data/chantiers/:ChantierId/", async (req, res, next) => {
+    const { type, ChantierId} = req.params;
+    const { date } = req.body;
+    try {
+        const etapes = await getEtapes(type,ChantierId,null,date)
+        res.json({etapes : etapes});
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.get("/:type/data/chantiers/:ChantierId/camionneurs/:CamionneurId", async (req, res, next) => {
+    const { type, ChantierId , CamionneurId} = req.params;
+    const { date } = req.body;
+    try {
+        const etapes = await getEtapes(type,ChantierId,CamionneurId,date)
+        res.json({etapes : etapes});
+    } catch (error) {
+        next(error)
+    }
+});
+
+    module.exports = router;
