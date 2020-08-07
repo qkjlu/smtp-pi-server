769ebdc1de6c300688f7a4d556a3db8018c51b6e
Lucas <lucas_mas@msn.com>
2020-08-07 12:55:31 +0200

    fix bearing query parameters not corresponding to number of waypoints

--- routes/directions.js
+++ routes/directions.js
@@ -7,9 +7,12 @@ const { directions } = require(".");
 
 router.get("/v5/gh/car/:coordinates", async (req, res, next) => {
   try {
-    let { coordinates } = req.params;
-    const response = await fetch("https://graphhopper.com/api/1/navigate/directions/v5/gh/car/"+ coordinates +"?access_token=pk.3dcbb461-4602-4a26-85e4-d386678d019e&geometries=polyline6&overview=full&steps=true&bearings=%3B%3B%3B&continue_straight=false&annotations=congestion%2Cdistance&language=fr&roundabout_exits=true&voice_instructions=true&banner_instructions=true&voice_units=metric&enable_refresh=true");
+    console.log(req.query);
+    const { bearings } = req.query;
+    const { coordinates } = req.params;
+    const response = await fetch("https://graphhopper.com/api/1/navigate/directions/v5/gh/car/"+ coordinates +"?access_token=pk.3dcbb461-4602-4a26-85e4-d386678d019e&geometries=polyline6&overview=full&steps=true&bearings="+bearings+"&continue_straight=false&annotations=congestion%2Cdistance&language=fr&roundabout_exits=true&voice_instructions=true&banner_instructions=true&voice_units=metric&enable_refresh=true");
     const direction = await response.json();
+    console.log(direction)
     const legs = direction.routes[0].legs;
 
     let flattenSteps = [];
